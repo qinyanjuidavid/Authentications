1f3bc31d5a2ae65c80e9ff7b33a626f16bada3be
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-03-26 10:29:03 +0300

    Initial commit

---
+++ lib/domain/auth/email_address.dart
@@ -0,0 +1,33 @@
+class EmailAddress {
+  final String value;
+  factory EmailAddress(String input) {}
+
+  const EmailAddress._(this.value) : assert(value != null);
+
+  @override
+  String toString() => 'EmailAddress($value)';
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+    return o is EmailAddress && o.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+}
+
+String validateEmailAddress(String input) {
+  const emailRegex =
+      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+  if (RegExp(emailRegex).hasMatch(input)) {
+    return input;
+  } else {
+    throw InvalidEmailException(input);
+  }
+}
+
+class InvalidEmailException implements Exception {
+  final String failedValue;
+  InvalidEmailException(this.failedValue);
+}
